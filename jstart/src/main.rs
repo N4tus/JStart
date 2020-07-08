e9a44b62d496067ad14eee3266e68add310dfa68
N4tus <mueker.ben@gmail.com>
2020-07-08 14:40:41 +0200

    switch to jdk calls

--- jstart/src/main.rs
+++ jstart/src/main.rs
@@ -1,9 +1,18 @@
-
-fn main() -> std::io::Result<()>{
+fn main() -> std::io::Result<()> {
     let launch_data = std::fs::read("launcher")?;
     let launch_str = String::from_utf8_lossy(&launch_data);
     let commands: Vec<_> = launch_str.split(" ").collect();
-
-    let res = std::process::Command::new("printer.bat").args(&commands).spawn()?.wait()?;
+    let program = {
+        if cfg!(windows) {
+            "jdk/bin/java.exe"
+        } else if cfg!(macos) {
+            "jdk/Contents/Home/bin/java"
+        } else if cfg!(unix) {
+            "jdk/bin/java"
+        } else {
+            panic!("only windows, mac and unix is supported")
+        }
+    };
+    let res = std::process::Command::new(program).args(&commands).spawn()?.wait()?;
     std::process::exit(res.code().unwrap_or(0));
 }
