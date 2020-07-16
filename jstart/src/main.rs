c43314d02d33b5ab6ebea58c526056164ca6ecf3
N4tus <mueker.ben@gmail.com>
2020-07-16 20:09:45 +0200

    replace App_HOME after split

--- jstart/src/main.rs
+++ jstart/src/main.rs
@@ -3,15 +3,16 @@
 fn main() -> std::io::Result<()> {
     let launch_data = std::fs::read("launcher")?;
     let app_home_path = std::env::current_dir()?;
-    let launch_str = String::from_utf8_lossy(&launch_data).replace("%APP_HOME%", &format!("{}", app_home_path.parent().unwrap().display()));
-    let commands: Vec<_> = launch_str.split(" ").collect();
+    let home = &format!("{}", app_home_path.parent().unwrap().display());
+    let launch_str = String::from_utf8_lossy(&launch_data);
+    let commands: Vec<_> = launch_str.split(" ").map(|c| c.replace("%APP_HOME%", home)).collect();
     let program = {
         if cfg!(windows) {
             "jdk/bin/javaw.exe"
         } else if cfg!(macos) {
-            "jdk/Contents/Home/bin/javaw"
+            "jdk/Contents/Home/bin/java"
         } else if cfg!(unix) {
-            "jdk/bin/javaw"
+            "jdk/bin/java"
         } else {
             panic!("only windows, mac and unix is supported")
         }
